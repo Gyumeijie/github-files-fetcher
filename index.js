23492e3486a17c866801ebc85d168fadc94d13e9
Gyumeijie <18170879819@163.com>
2018-08-27 20:12:09 +0800

    refactor: use eslint to fix coding style

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var fs = require('fs');
-var os = require('os');
-var url = require('url');
-var axios = require('axios');
-var Promise = require('promise');
-var shell = require('shelljs');
-var save = require('save-file');
-var argsParser = require("args-parser");
+const fs = require('fs');
+const os = require('os');
+const url = require('url');
+const axios = require('axios');
+const Promise = require('promise');
+const shell = require('shelljs');
+const save = require('save-file');
+const argsParser = require('args-parser');
 
 const AUTHOR = 1;
 const REPOSITORY = 2;
@@ -13,28 +13,36 @@ const BRANCH = 4;
 
 // A utility function for expand `~`
 function tilde(pathString) {
-    if (pathString[0] === '~') {
-        return os.homedir() + pathString.substring(1);
-    } else {
-        return pathString;
-    }
+  if (pathString[0] === '~') {
+    return os.homedir() + pathString.substring(1);
+  }
+  return pathString;
 }
 
-
 // The default output directory is the current directory
-var outputDirectory = './';
+let outputDirectory = './';
 // Default authentication setting
-var authentication = {};
-var authenticationSwitch = {};
+let authentication = {};
+let authenticationSwitch = {};
 // Defalut configuration file
-var configFile = tilde("~/.download_github");
+let configFile = tilde('~/.download_github');
 
+function checkGithubRepoURLValidity(downloadUrl) {
+  const { hostname, pathname } = url.parse(downloadUrl, true);
+
+  if (hostname !== 'github.com') {
+    throw new Error('Invalid domain: github.com is expected!');
+  }
+
+  if (pathname.split('/').length < 3) {
+    throw new Error('Invalid url: https://github.com/user/repository is expected');
+  }
+}
 
 const args = argsParser(process.argv);
 (function tackleArgs() {
-
-    if (args.help) {
-        console.log(`
+  if (args.help) {
+    console.log(`
           Usage: download [OPTION]... 
           Example: download --url='https://github.com/user/repository'  --out='~/output'
           
@@ -53,306 +61,279 @@ const args = argsParser(process.argv);
           --file=config_file            the default configuration file is the '~/download_github'
         `);
 
-        return;
+    return;
+  }
+
+  // The url is required and should be a valid github repository url
+  if (!args.url) {
+    throw new Error('input a url');
+  } else {
+    checkGithubRepoURLValidity(args.url);
+  }
+
+  if (args.out) {
+    outputDirectory = tilde(args.out);
+    if (outputDirectory[args.out.length - 1] !== '/') {
+      outputDirectory = `${outputDirectory}/`;
+    }
+  }
+
+  if (args.auth) {
+    const { auth } = args;
+
+    const colonPos = auth.indexOf(':');
+    if (colonPos === -1 || colonPos === auth.length - 1) {
+      throw new Error('Bad auth option: username:password is expected!');
     }
 
-    // The url is required and should be a valid github repository url
-    if (!args.url) {
-        throw new Error("input a url")
-    } else {
-        checkGithubRepoURLValidity(args.url);
+    const [username, password] = auth.split(':');
+    authentication.auth = {
+      username,
+      password,
+    };
+
+    if (args.alwaysUseAuth) {
+      authenticationSwitch = authentication;
     }
+  }
 
-    if (args.out) {
-        outputDirectory = tilde(args.out);
-        if (outputDirectory[args.out.length-1] !== '/') {
-            outputDirectory = outputDirectory + "/";
-        }
-    }
+  if (args.file) {
+    configFile = tilde(args.file);
+  }
+}());
 
-    if (args.auth) {
-        let auth = args.auth;
-
-        let colonPos = auth.indexOf(':');
-        if ( colonPos==-1 || colonPos == auth.length-1) {
-            throw new Error("Bad auth option: username:password is expected!");
-        }
-
-        [username, password] = auth.split(':');
-        authentication.auth = {
-            username,
-            password
-        };
-
-        if (args.alwaysUseAuth) {
-            authenticationSwitch = authentication;
-        }
-    }
-
-    if (args.file) {
-        configFile = tilde(args.file);
-    }
-
-})();
-
-function checkGithubRepoURLValidity(downloadUrl) {
-       var {hostname, pathname} = url.parse(downloadUrl, true);
-
-       if (hostname !== "github.com") {
-           throw new Error("Invalid domain: github.com is expected!")
-       }
-
-       if (pathname.split('/').length < 3) {
-           throw new Error("Invalid url: https://github.com/user/repository is expected")
-       }
-}
-
-var parameters = {
-    url: args.url,
-    fileName: undefined,
-    rootDirectory: undefined
+const parameters = {
+  url: args.url,
+  fileName: undefined,
+  rootDirectory: undefined,
 };
-
 
 // If no command line authentication provided, read the configuration file
 if (!authentication.auth) {
-    (function parseConfig(){
-        var exists = fs.existsSync(configFile);
-        if (exists) {
-            var data = fs.readFileSync(configFile, 'utf8');
-            authentication = JSON.parse(data);
+  (function parseConfig() {
+    const isExistent = fs.existsSync(configFile);
+    if (isExistent) {
+      const data = fs.readFileSync(configFile, 'utf8');
+      authentication = JSON.parse(data);
 
-            if (args.alwaysUseAuth) {
-                authenticationSwitch = authentication;
-            }
-        } else {
-            console.warn("No configuration file provided!");
-        }
-    })();
-}
-
-
-function parseInfo(parameters) {
-
-    var repoPath = url.parse(parameters.url, true).pathname;
-    var splitPath = repoPath.split("/");
-    var info = {};
-
-    info.author = splitPath[AUTHOR];
-    info.repository = splitPath[REPOSITORY];
-    info.branch = splitPath[BRANCH];
-    info.rootName = splitPath[splitPath.length-1];
-
-    info.urlPrefix = `https://api.github.com/repos/${info.author}/${info.repository}/contents/`;
-    info.urlPostfix = `?ref=${info.branch}`;
-
-    if(!!splitPath[BRANCH]){
-        info.resPath = repoPath.substring(repoPath.indexOf(splitPath[BRANCH])+splitPath[BRANCH].length+1);
-    }
-
-    if(!parameters.fileName || parameters.fileName==""){
-        info.downloadFileName = info.rootName;
+      if (args.alwaysUseAuth) {
+        authenticationSwitch = authentication;
+      }
     } else {
-        info.downloadFileName = parameters.fileName;
+      console.warn('No configuration file provided!');
     }
-
-    if(parameters.rootDirectory == "false"){
-        info.rootDirectoryName = "";
-    } else if (!parameters.rootDirectory || parameters.rootDirectory=="" ||
-        parameters.rootDirectory=="true"){
-        info.rootDirectoryName = info.rootName+"/";
-    } else {
-        info.rootDirectoryName = parameters.rootDirectory+"/";
-    }
-
-    return info;
+  }());
 }
 
+function parseInfo(repoInfo) {
+  const repoPath = url.parse(repoInfo.url, true).pathname;
+  const splitPath = repoPath.split('/');
+  const info = {};
 
-var basicOptions = {
-    method: "get",
-    responseType: 'arrayBuffer'
-};
+  info.author = splitPath[AUTHOR];
+  info.repository = splitPath[REPOSITORY];
+  info.branch = splitPath[BRANCH];
+  info.rootName = splitPath[splitPath.length - 1];
 
-function downloadDirectory(){
+  // Common parts of url for downloading
+  info.urlPrefix = `https://api.github.com/repos/${info.author}/${info.repository}/contents/`;
+  info.urlPostfix = `?ref=${info.branch}`;
 
-    var dirPaths = [];
-    var files = [];
-    var requestPromises = [];
+  if (splitPath[BRANCH]) {
+    info.resPath = repoPath.substring(repoPath.indexOf(splitPath[BRANCH]) + splitPath[BRANCH].length + 1);
+  }
 
-    dirPaths.push(repoInfo.resPath);
-    iterateDirectory(dirPaths, files, requestPromises);
-}
+  if (!repoInfo.fileName || repoInfo.fileName === '') {
+    info.downloadFileName = info.rootName;
+  } else {
+    info.downloadFileName = repoInfo.fileName;
+  }
 
-function iterateDirectory(dirPaths, files, requestPromises){
+  if (repoInfo.rootDirectory === 'false') {
+    info.rootDirectoryName = '';
+  } else if (!repoInfo.rootDirectory || repoInfo.rootDirectory === ''
+  || repoInfo.rootDirectory === 'true') {
+    info.rootDirectoryName = `${info.rootName}/`;
+  } else {
+    info.rootDirectoryName = `${parameters.rootDirectory}/`;
+  }
 
-    axios({
-        ...basicOptions,
-        url: repoInfo.urlPrefix+dirPaths.pop()+repoInfo.urlPostfix,
-        ...authenticationSwitch
-    }).then(function(response) {
-
-        for(var i=0; i<response.data.length; i++){
-            if(response.data[i].type == "dir"){
-                dirPaths.push(response.data[i].path);
-            } else {
-                if(response.data[i].download_url) {
-                    var promise = fetchFile(response.data[i].path, response.data[i].download_url, files);
-                    requestPromises.push(promise);
-                } else {
-                    console.log(response.data[i]);
-                }
-            }
-        }
-
-        // Save files after we iterate all the directories
-        if(dirPaths.length <= 0){
-            saveFiles(files, requestPromises);
-        } else {
-            iterateDirectory(dirPaths, files, requestPromises);
-        }
-    }).catch(function(error){
-        processClientError(error, iterateDirectory.bind(null, dirPaths, files, requestPromises));
-    });
+  return info;
 }
 
 function extractFilenameAndDirectoryFrom(path) {
+  const components = path.split('/');
+  const filename = components[components.length - 1];
+  const directory = path.substring(0, path.length - filename.length);
 
-     var components = path.split('/');
-     var filename = components[components.length-1];
-     var directory = path.substring(0, path.length-filename.length);
-
-     return {
-         filename: filename,
-         directory: directory
-     };
+  return {
+    filename,
+    directory,
+  };
 }
 
-function saveFiles(files, requestPromises){
+const basicOptions = {
+  method: 'get',
+  responseType: 'arrayBuffer',
+};
+// Global variable
+let repoInfo = {};
 
-    var rootDir = outputDirectory + repoInfo.rootDirectoryName;
-    shell.mkdir('-p', rootDir);
+function saveFiles(files, requestPromises) {
+  const rootDir = outputDirectory + repoInfo.rootDirectoryName;
+  shell.mkdir('-p', rootDir);
 
-    Promise.all(requestPromises).then(function(data) {
-
-        for(let i=0; i<files.length; i++) {
-
-            var pathForSave = extractFilenameAndDirectoryFrom(files[i].path.substring(decodeURI(repoInfo.resPath).length+1));
-            var dir = rootDir + pathForSave.directory;
-
-            fs.exists(dir, function (i,dir, pathForSave, exists) {
-                if (!exists) {
-                    shell.mkdir('-p', dir);
-                }
-                save(files[i].data, dir + pathForSave.filename, (err, data) => {
-                    if (err) throw err;
-                })
-            }.bind(null, i, dir, pathForSave));
-         }
-    });
+  Promise.all(requestPromises).then(() => {
+    for (let i = 0; i < files.length; i++) {
+      const pathForSave = extractFilenameAndDirectoryFrom(files[i].path.substring(decodeURI(repoInfo.resPath).length + 1));
+      const dir = rootDir + pathForSave.directory;
+      fs.exists(dir, ((i, dir, pathForSave, exists) => {
+        if (!exists) {
+          shell.mkdir('-p', dir);
+        }
+        save(files[i].data, dir + pathForSave.filename, (err) => {
+          if (err) throw err;
+        });
+      }).bind(null, i, dir, pathForSave));
+    }
+  });
 }
 
 function processClientError(error, retryCallback) {
-    if (error.response.status == "401") {
-        // Unauthorized
-        console.error("Bad credentials, please check your username or password(or access token)!");
-    } else if (error.response.status == "403") {
-
-        if (authentication.auth) {
-            // If the default API access rate without authentication exceeds and the command line
-            // authentication is provided, then we switch to use authentication
-            console.warn("The unauthorized API access rate exceeded, we are now retrying with authentication......");
-            authenticationSwitch = authentication;
-            retryCallback();
-        } else {
-            // API rate limit exceeded
-            console.error("API rate limit exceeded, Authenticated requests get a higher rate limit." +
-                " Check out the documentation for more details. https://developer.github.com/v3/#rate-limiting");
-        }
+  if (error.response.status === '401') {
+    // Unauthorized
+    console.error('Bad credentials, please check your username or password(or access token)!');
+  } else if (error.response.status === '403') {
+    if (authentication.auth) {
+      // If the default API access rate without authentication exceeds and the command line
+      // authentication is provided, then we switch to use authentication
+      console.warn('The unauthorized API access rate exceeded, we are now retrying with authentication......');
+      authenticationSwitch = authentication;
+      retryCallback();
     } else {
-        console.error(error.message);
+      // API rate limit exceeded
+      console.error('API rate limit exceeded, Authenticated requests get a higher rate limit.'
+                  + ' Check out the documentation for more details. https://developer.github.com/v3/#rate-limiting');
     }
+  } else {
+    console.error(error.message);
+  }
 }
-function fetchFile(path, url, files) {
 
-    return axios({
-            ...basicOptions,
-            url,
-            ...authenticationSwitch
-        }).then(function (file) {
-            console.log("downloading ", path);
-            files.push({path: path, data: file.data});
-        }).catch(function(error) {
-            processClientError(error, fetchFile.bind(null, path, url, files));
-        });
+function fetchFile(path, url, files) {
+  return axios({
+    ...basicOptions,
+    url,
+    ...authenticationSwitch,
+  }).then((file) => {
+    console.log('downloading ', path);
+    files.push({ path, data: file.data });
+  }).catch((error) => {
+    processClientError(error, fetchFile.bind(null, path, url, files));
+  });
 }
 
 function downloadFile(url) {
+  console.log('downloading ', repoInfo.resPath);
 
-    console.log("downloading ", repoInfo.resPath);
+  axios({
+    ...basicOptions,
+    url,
+    ...authenticationSwitch,
+  }).then((file) => {
+    shell.mkdir('-p', outputDirectory);
+    const pathForSave = extractFilenameAndDirectoryFrom(decodeURI(repoInfo.resPath));
 
-    axios({
-        ...basicOptions,
-        url,
-        ...authenticationSwitch
-    }).then(function (file) {
-        shell.mkdir('-p', outputDirectory);
-        var pathForSave = extractFilenameAndDirectoryFrom(decodeURI(repoInfo.resPath));
-
-        save(file.data, outputDirectory + pathForSave.filename, (err, data) => {
-            if (err) throw err;
-        })
-    }).catch(function(error){
-        processClientError(error, downloadFile.bind(null, url));
+    save(file.data, outputDirectory + pathForSave.filename, (err) => {
+      if (err) throw err;
     });
+  }).catch((error) => {
+    processClientError(error, downloadFile.bind(null, url));
+  });
 }
 
-var repoInfo = {};
-function initializeDownload(parameters) {
-    repoInfo = parseInfo(parameters);
-
-    if(!repoInfo.resPath || repoInfo.resPath==""){
-        if(!repoInfo.branch || repoInfo.branch==""){
-            repoInfo.branch = "master";
-        }
-
-        // Download the whole repository
-        var repoUrl = `https://github.com/${repoInfo.author}/${repoInfo.repository}/archive/${repoInfo.branch}.zip`;
-
-        axios({
-             ...basicOptions,
-             responseType: 'stream',
-             url: repoUrl,
-             ...authenticationSwitch
-        }).then(function(response){
-             shell.mkdir('-p', outputDirectory);
-             var filename = outputDirectory + `${repoInfo.repository}.zip`;
-             response.data.pipe(fs.createWriteStream(filename))
-                 .on('close', function () {
-                     console.log(`${filename} downloaded.`);
-                 });
-        }).catch(function(error) {
-             processClientError(error, initializeDownload.bind(null, parameters));
-        });
-    } else {
-        // Download part of repository
-        axios({
-            ...basicOptions,
-            url: repoInfo.urlPrefix+repoInfo.resPath+repoInfo.urlPostfix,
-            ...authenticationSwitch
-        }).then(function(response) {
-            if(response.data instanceof Array){
-                downloadDirectory();
-            } else {
-                downloadFile(response.data.download_url);
-            }
-        }).catch(function(error) {
-            processClientError(error, initializeDownload.bind(null, parameters));
-        });
+function iterateDirectory(dirPaths, files, requestPromises) {
+  axios({
+    ...basicOptions,
+    url: repoInfo.urlPrefix + dirPaths.pop() + repoInfo.urlPostfix,
+    ...authenticationSwitch,
+  }).then((response) => {
+    const { data } = response;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].type === 'dir') {
+        dirPaths.push(data[i].path);
+      } else if (data[i].download_url) {
+        const promise = fetchFile(data[i].path, data[i].download_url, files);
+        requestPromises.push(promise);
+      } else {
+        console.log(data[i]);
+      }
     }
+
+    // Save files after we iterate all the directories
+    if (dirPaths.length === 0) {
+      saveFiles(files, requestPromises);
+    } else {
+      iterateDirectory(dirPaths, files, requestPromises);
+    }
+  }).catch((error) => {
+    processClientError(error, iterateDirectory.bind(null, dirPaths, files, requestPromises));
+  });
+}
+
+function downloadDirectory() {
+  const dirPaths = [];
+  const files = [];
+  const requestPromises = [];
+
+  dirPaths.push(repoInfo.resPath);
+  iterateDirectory(dirPaths, files, requestPromises);
+}
+
+function initializeDownload(para) {
+  repoInfo = parseInfo(para);
+
+  if (!repoInfo.resPath || repoInfo.resPath === '') {
+    if (!repoInfo.branch || repoInfo.branch === '') {
+      repoInfo.branch = 'master';
+    }
+
+    // Download the whole repository
+    const repoURL = `https://github.com/${repoInfo.author}/${repoInfo.repository}/archive/${repoInfo.branch}.zip`;
+
+    axios({
+      ...basicOptions,
+      responseType: 'stream',
+      url: repoURL,
+      ...authenticationSwitch,
+    }).then((response) => {
+      shell.mkdir('-p', outputDirectory);
+      const filename = `${outputDirectory}${repoInfo.repository}.zip`;
+      response.data.pipe(fs.createWriteStream(filename))
+        .on('close', () => {
+          console.log(`${filename} downloaded.`);
+        });
+    }).catch((error) => {
+      processClientError(error, initializeDownload.bind(null, parameters));
+    });
+  } else {
+    // Download part(s) of repository
+    axios({
+      ...basicOptions,
+      url: repoInfo.urlPrefix + repoInfo.resPath + repoInfo.urlPostfix,
+      ...authenticationSwitch,
+    }).then((response) => {
+      if (response.data instanceof Array) {
+        downloadDirectory();
+      } else {
+        downloadFile(response.data.download_url);
+      }
+    }).catch((error) => {
+      processClientError(error, initializeDownload.bind(null, parameters));
+    });
+  }
 }
 
 if (!args.help) {
-    initializeDownload(parameters);
+  initializeDownload(parameters);
 }
-
